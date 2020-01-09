252b2142ad4756e6f7c71239fe00d88595ad95d8
9oominsoo <rnasltn@ajou.ac.kr>
2020-01-09 16:17:02 +0900

    Environment Setting(springboot gradle-jpa-mysql)

---
+++ src/main/java/com/ad/apiserver/ApiserverApplication.java
@@ -0,0 +1,20 @@
+package com.ad.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(
+        exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class ApiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ad/apiserver/ApiserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
